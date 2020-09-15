64b4b23a765a8218ffd6b525ca995e9ed0e6c024
pussy_lover214 <allanbilfaqih214@users.noreply.github.com>
2020-09-15 20:53:36 +0700

    testLKS1
    test file lks1

---
+++ Test2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test2
+{
+    public partial class Form1 : Form
+    {
+        public int role;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void showForm()
+        {
+            formMain main = new formMain(this);
+            main.Show();
+            this.Visible = false;
+        }
+        void auth()
+        {
+            using (squekEnt sqlData = new squekEnt())
+            {
+                var counting = sqlData.Karyawans.Count(i => i.Username == textBox1.Text && i.Password == textBox2.Text);
+                if (counting == 1)
+                {
+                    var getinfo = sqlData.Karyawans.FirstOrDefault(i => i.Username == textBox1.Text);
+                    role = getinfo.Role;
+                    showForm();
+                }
+                else
+                {
+                    MessageBox.Show("Salah");
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            auth();
+        }
+    }
+}
---
+++ Test2/formMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test2
+{
+    public partial class formMain : Form
+    {
+        Form1 formLogin;
+        public formMain(Form1 form1)
+        {
+            InitializeComponent();
+            this.formLogin = form1;
+        }
+
+        private void formMain_Load(object sender, EventArgs e)
+        {
+            if (formLogin.role == 1)
+            {
+                MessageBox.Show("Full");
+                button1.Visible = true;
+                button2.Visible = false;
+            }    
+            else if (formLogin.role == 0)
+            {
+                MessageBox.Show("Half");
+                button1.Visible = false;
+                button2.Visible = true;
+            }
+        }
+    }
+}
